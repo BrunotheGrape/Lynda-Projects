54236065beb5870c36e97561eed7128ccd30399c
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-09-19 19:38:08 -0500

    Lynda Descriptive Stats Challenge

---
+++ RET_CPTR3_CHALLENGE.R
@@ -0,0 +1,11 @@
+#Lynda RET Chapter 3 Challenge
+data(mtcars) #open existing R data set
+summary(mtcars)
+
+
+mpg <- mtcars[1] #put mpg in a seperate table
+hp <- mtcars[4] #put horsepowere in a seperate table
+qsec <- mtcars[7] #put qsec in a seperate table
+describe(mpg)
+describe(hp)
+describe(qsec)
