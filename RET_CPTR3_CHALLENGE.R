95bf80139b513c5908cff2393ccd111d3ed00eb7
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-09-19 19:47:43 -0500

    chapter 3 challenge final version

--- RET_CPTR3_CHALLENGE.R
+++ RET_CPTR3_CHALLENGE.R
@@ -9,3 +9,7 @@ qsec <- mtcars[7] #put qsec in a seperate table
 describe(mpg)
 describe(hp)
 describe(qsec)
+
+# solution
+d <- describe(mtcars[c(1, 4, 7)]) #this pulls the stats we want into the object d
+d[, c(3, 4, 11, 12)] # this prints out all rows in the results because rows are not stated. it prints out the 3rd, 4th, 11th, and 12th column which are the stats we are looking for.
